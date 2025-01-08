86f44205cb5865f8bb2c6b6ee2c3f5e952d02420
FabricioHiury <fabhiury@gmail.com>
2025-01-07 22:40:15 -0300

    Feat: making public and private keys for authenticate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
+import { AuthenticateController } from './controllers/authenticate-controller';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { envSchema } from './env';
       isGlobal: true,
     }),
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
